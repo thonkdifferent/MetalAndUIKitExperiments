6133469b61fd148c3c014988a0ffadc26486bb30
ThonkDifferent <41342923+thonkdifferent@users.noreply.github.com>
2023-03-22 13:56:17 +0200

    Initial Commit

---
+++ UIKitMadness/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  UIKitMadness
+//
+//  Created by Bogdan Petru on 08.03.2023.
+//
+
+import UIKit
+import Metal
+
+class ViewController: UIViewController {
+
+    var device: MTLDevice!
+    var metalLayer: CAMetalLayer!
+    var vertexBuffer: MTLBuffer!
+    let vertexData: [Float] = [
+        0.0,1.0,0.0,
+        -1.0,-1.0,0.0,
+        1.0,-1.0,0.0
+    ]
+    var pipelineState: MTLRenderPipelineState!
+    var commandQueue: MTLCommandQueue!
+    
+    var timer: CADisplayLink!
+    
+    func render()
+    {
+        guard let drawable = metalLayer?.nextDrawable() else {return}
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(
+            red:0.0,
+            green: 0.0,
+            blue:0.0,
+            alpha:1.0
+        )
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        
+        let renderEncoder = commandBuffer!.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+        
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3,instanceCount: 1)
+        renderEncoder.endEncoding()
+        
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
+        
+    }
+    @objc func gameloop()
+    {
+        autoreleasepool()
+        {
+            self.render()
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        device = MTLCreateSystemDefaultDevice()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+        
+        
+        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData)
+        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize,options: [])
+        
+        
+        let defaultLibrary = device.makeDefaultLibrary()!
+        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment");
+        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex");
+        
+        let pipelineStateDescriptior = MTLRenderPipelineDescriptor()
+        
+        pipelineStateDescriptior.vertexFunction = vertexProgram
+        pipelineStateDescriptior.fragmentFunction = fragmentProgram
+        pipelineStateDescriptior.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptior)
+        
+        commandQueue = device.makeCommandQueue()
+        
+        
+        timer = CADisplayLink(target: self, selector: #selector(gameloop))
+        timer.add(to: RunLoop.main, forMode: .default)
+    }
+
+
+}
+
---
+++ UIKitMadness/shader.metal
@@ -0,0 +1,21 @@
+//
+//  shader.metal
+//  UIKitMadness
+//
+//  Created by Bogdan Petru on 22.03.2023.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+vertex float4 basic_vertex(
+const device packed_float3* vertex_array [[  buffer(0)  ]],
+unsigned int vid [[ vertex_id  ]])
+{
+    return float4(vertex_array[vid],1.0);
+}
+
+fragment half4 basic_fragment()
+{
+    return half4(1.0);
+}
